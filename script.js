ec92d09498443e7c0913cf25cef6bfc446a2def1
Atimada <carlbadakou@gmail.com>
2025-01-28 12:00:09 +0000

    Ajout des fichiers à l'index

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+console.log("Le fichier script.js est chargé !");
+
+function afficherMessage() {
+    console.log("Ceci est un message depuis la fonction afficherMessage().");
+}
+
+afficherMessage();
